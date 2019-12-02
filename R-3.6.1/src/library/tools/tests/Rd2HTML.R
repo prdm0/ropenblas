1b12a5aae8a3bbd8a7793142186b79e30c8eb678
Pedro Rafael <pedro.rafael.marinho@gmail.com>
2019-12-02 10:18:41 -0300

    Update download_r() functio.

--- R-3.6.1/src/library/tools/tests/Rd2HTML.R
+++
@@ -1,6 +0,0 @@
-x <- tools::Rd_db("base")
-system.time(y <- lapply(x, function(e)
-    tryCatch(tools::Rd2HTML(e, out = nullfile()),
-	     error = identity))) # 3-5 sec
-stopifnot(!vapply(y, inherits, NA, "error"))
-## Gave error when "running" \Sexpr{.} DateTimeClasses.Rd
