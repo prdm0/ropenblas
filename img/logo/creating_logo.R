88b5f893708ee90823c677fca4826ebba13c6bdb
prdm0 <pedro.rafael.marinho@gmail.com>
2021-04-05 23:58:41 -0300

    :art: Update of the logo and code that generates the logo
    Hex Logo update and code update for automatic logo generation.

--- img/logo/creating_logo.R
+++ img/logo/creating_logo.R
@@ -1,8 +1,40 @@
-# remotes::install_github("GuangchuangYu/hexSticker")
+# remotes::install_github("GuangchuangYu/hexSticker", force = TRUE)
 
 library(hexSticker)
+library(ggplot2)
+library(glue)
+library(magick)
+library(fs)
 
-imgurl <- system.file("~/Downloads/ropenblas/inst/openblas_logo_01.png", package="hexSticker")
+img <- image_read(path(getwd(), "img", "logo", "img.png"))
+logo <- image_ggplot(img, interpolate = TRUE)
 
-sticker("~/Downloads/ropenblas/inst/logo/molde.png", package="", p_size=22, s_x=1.03, s_y=.95, s_width=1,
-        filename="openblas_logo.png", h_fill="#000000", h_color="#929292", white_around_sticker = T)
+s <- sticker(
+  logo,
+  package = "ropenblas",
+  p_size = 20,
+  s_width = 1.3,
+  s_height = 1.3,
+  s_x = 1.1,
+  s_y = 0.84,
+  h_fill = "#FE5750",
+  h_color = "#E8DF9C",
+  p_color = "#0F2536",
+  p_family = "Aller_Rg",
+  h_size = 2,
+  white_around_sticker = T,
+  filename = path(getwd(), "img", "logo", "logo.png"),
+  url = "https://prdm0.github.io/ropenblas/",
+  u_size = 4.5,
+  spotlight = T,
+  l_alpha = 0.4,
+  dpi = 300,
+  u_color = "white"
+)
+
+fs::file_delete(fs::path(getwd(), "logo.png"))
+
+fs::file_copy(
+  path = path(getwd(), "img", "logo", "logo.png"),
+  new_path = path(getwd(), "logo.png")
+)
